6c706be60b7ce8681ec6b9596d38f5ded8eb4ca1
Moritz Gellner <moritz.gellner@gmail.com>
2014-03-19 04:46:15 -0500

    standardize base script invocation syntax

--- get_ingredients_directions.py
+++ get_ingredients_directions.py
@@ -1,60 +1,77 @@
 #import urllib2
-import requests
+import requests, sys
 from bs4 import BeautifulSoup
 from recipe import Recipe, Ingredient
 
-resp = requests.get('http://allrecipes.com/Recipe/Easy-Tuna-Patties/Detail.aspx?soid=carousel_0_rotd&prop24=rotd')
-#soup = BeautifulSoup(urllib2.urlopen(str(sys.argv[0])).read())
-soup = BeautifulSoup(resp.text.lower(),"lxml")
+DEBUG = True
 
-ingredient_tags = soup.find_all(class_="fl-ing")
-ingredients = []
+if __name__ == "__main__":
 
-for ingr in ingredient_tags:
-    chunks = ingr.text.lstrip('\n').rstrip('\n').split('\n')
-    amt_unit= chunks[0].split()
-    print amt_unit
-    if len(amt_unit)>1:
-        amt = amt_unit[0][0]
-        unit = ""
-        for i in range(1,len(amt_unit)):
-            unit += amt_unit[i]
-        print "amt is: " + str(amt)
-        print "unit is:" + str(unit) 
+    OUTPUT_FORMAT = 'print'
+
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        if '--output=json' in sys.argv:
+            output_format = 'json'
     else:
-        amt = amt_unit[0]
-        unit = "unit"
-        print "amt is: " + str(amt)
-        print "unit is:" + str(unit) 
-    ingredients.append(Ingredient(chunks[1],amt, unit))
-	
+        url = 'http://allrecipes.com/Recipe/Easy-Tuna-Patties/Detail.aspx?soid=carousel_0_rotd&prop24=rotd'
 
-direction_tags = soup.find_all(class_="plaincharacterwrap break")
-all_directions = ""
-for d in direction_tags:
-	all_directions += d.text
 
-recipe = Recipe(ingredients,all_directions)
+    resp = requests.get(url)
+    #soup = BeautifulSoup(urllib2.urlopen(str(sys.argv[0])).read())
+    soup = BeautifulSoup(resp.text.lower(),"lxml")
 
-# print recipe
+    ingredient_tags = soup.find_all(class_="fl-ing")
+    ingredients = []
 
-# print recipe.jsonify()
+    for ingr in ingredient_tags:
+        chunks = ingr.text.lstrip('\n').rstrip('\n').split('\n')
+        amt_unit= chunks[0].split()
+        print amt_unit
+        if len(amt_unit)>1:
+            amt = amt_unit[0][0]
+            unit = ""
+            for i in range(1,len(amt_unit)):
+                unit += amt_unit[i]
+            print "amt is: " + str(amt)
+            print "unit is:" + str(unit) 
+        else:
+            amt = amt_unit[0]
+            unit = "unit"
+            print "amt is: " + str(amt)
+            print "unit is:" + str(unit) 
+        ingredients.append(Ingredient(chunks[1],amt, unit))
+    	
 
-choice = raw_input("What transformation do you want to do? (--help for help): ") # 
-if choice == '--help':
-    print "1: [to|from] vegetarian"
+    direction_tags = soup.find_all(class_="plaincharacterwrap break")
+    all_directions = ""
+    for d in direction_tags:
+    	all_directions += d.text
 
-else:
-    chunks = choice.split(' ')
-    direction = chunks[0]
-    dimension = chunks[1]
-    print "dimension: " + dimension
-    print "direction: " + direction
+    recipe = Recipe(ingredients,all_directions)
 
-    recipe.transform(dimension,direction)
+    # print recipe
 
-    print recipe
-    print recipe.jsonify()
+    # print recipe.jsonify()
 
-#json_output = recipe.jsonify()
-#print json_output
+    choice = raw_input("What transformation do you want to do? (--help for help): ") # 
+    if choice == '--help':
+        print "1: [to|from] vegetarian"
+
+    else:
+        chunks = choice.split(' ')
+        direction = chunks[0]
+        dimension = chunks[1]
+        if DEBUG:
+            print "dimension: " + dimension
+            print "direction: " + direction
+
+        recipe.transform(dimension,direction)
+
+        if OUTPUT_FORMAT == 'print':
+            print recipe
+        else:
+            print recipe.jsonify()
+
+    #json_output = recipe.jsonify()
+    #print json_output
